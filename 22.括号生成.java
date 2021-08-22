4184b1b9d830efc3e819440ff1ebb7384d2c1754
zhhades <zhhades@foxmail.com>
2021-08-22 21:33:43 +0800

    init code

---
+++ 22.括号生成.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=22 lang=java
+ *
+ * [22] 括号生成
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> generateParenthesis(int n) {
+
+    }
+}
+// @lc code=end
+
