133efdf8b350b9315e59afa9497ce24ae29bc7ef
zhhades <zhhades@foxmail.com>
2021-08-22 21:57:44 +0800

    init leetcode

--- 1.两数之和.java
+++ 1.两数之和.java
@@ -7,7 +7,18 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int length = nums.length;
+        for(int i=0;i<length;i++){
+            map.put(nums[i],i);
+        }
+        for(int i=0;i<length;i++){
+            Integer right = map.get(target-nums[i]);
+            if(right != null && right != i){
+                return new int[]{i,right};
+            }
+        }
+        return new int[2];
     }
 }
 // @lc code=end
